29df2e6f217fb455a9e2b882f8317fa1fc731ed9
Bill Moran <wmoran@potentialtech.com>
2022-01-14 11:37:06 -0500

    More better Offer test (with assertion)

--- market/market_test.go
+++ market/market_test.go
@@ -3,9 +3,13 @@ package market
 import "testing"
 
 func TestOffer(t *testing.T) {
-	m := MakeMarket(makeMemoryMarketStorage())
+	storage := makeMemoryMarketStorage()
+	m := MakeMarket(storage)
 	o := Offer{Symbol: "m"}
 	m.Offer(o)
+	if len(storage.offers["m"]) != 1 {
+		t.Fatalf("%+v", storage)
+	}
 }
 
 func TestBid(t *testing.T) {
