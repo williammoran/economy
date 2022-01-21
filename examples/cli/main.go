b3ba75ee4fa1b17f8c878297ffb7128f383abac5
Bill Moran <wmoran@potentialtech.com>
2022-01-21 14:23:25 -0500

    Basic working example

---
+++ examples/cli/main.go
@@ -0,0 +1,179 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/williammoran/economy"
+)
+
+func main() {
+	market := economy.MakeMarket(makeMemoryMarketStorage())
+	accounts := make(map[int64]int64)
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		tokens := nextCommand(reader)
+		if len(tokens) == 0 {
+			continue
+		}
+		command := strings.ToLower(tokens[0])
+		switch command {
+		case "account":
+			setAccount(tokens[1:], accounts)
+		case "accounts":
+			showAccounts(accounts)
+		case "bid":
+			bid(tokens[1:], market)
+		case "offer":
+			offer(tokens[1:], market)
+		case "market":
+			showMarket(market)
+		default:
+			fmt.Printf("Unrecognized command '%s'\n", command)
+		}
+	}
+}
+
+func nextCommand(r io.ByteReader) []string {
+	var buf []byte
+	for {
+		c, err := r.ReadByte()
+		if err != nil {
+			break
+		}
+		if c == '\n' {
+			break
+		}
+		if c != '\r' {
+			buf = append(buf, c)
+		}
+	}
+	c := strings.TrimSpace(string(buf))
+	space := regexp.MustCompile(`\s+`)
+	c = space.ReplaceAllString(c, " ")
+	return strings.Split(c, " ")
+}
+
+func setAccount(c []string, accounts map[int64]int64) {
+	id, ok := parseAccount(c[0])
+	if !ok {
+		return
+	}
+	amount, ok := parseAmount(c[1])
+	if !ok {
+		return
+	}
+	accounts[id] = amount
+	fmt.Printf("Account %d now has %d\n", id, amount)
+}
+
+func showAccounts(accounts map[int64]int64) {
+	fmt.Println("AccountID   Balance")
+	for id, balance := range accounts {
+		fmt.Printf("%9d %9d\n", id, balance)
+	}
+}
+
+// bid $account $symbol $volume
+// bid $account $symbol $volume limit $price
+func bid(c []string, market *economy.Market) {
+	bidType, account, symbol, volume, price, ok := parseBidOrOffer(c)
+	if !ok {
+		return
+	}
+	bid := economy.Bid{
+		BidType: bidType,
+		Account: account,
+		Symbol:  symbol,
+		Price:   price,
+		Amount:  volume,
+	}
+	market.Bid(bid)
+	fmt.Printf("Made bid %+v\n", bid)
+}
+
+// offer $account $symbol $volume
+// offer $account $symbol $volume limit $price
+func offer(c []string, market *economy.Market) {
+	offerType, account, symbol, volume, price, ok := parseBidOrOffer(c)
+	if !ok {
+		return
+	}
+	offer := economy.Offer{
+		OfferType: offerType,
+		Account:   account,
+		Symbol:    symbol,
+		Price:     price,
+		Amount:    volume,
+	}
+	market.Offer(offer)
+	fmt.Printf("Made offer %+v\n", offer)
+}
+
+// $_ $account $symbol $volume
+// $_ $account $symbol $volume limit $price
+func parseBidOrOffer(c []string) (economy.OrderType, int64, economy.Symbol, int64, int64, bool) {
+	var ok bool
+	var orderType economy.OrderType
+	var price int64
+	if len(c) == 3 {
+		orderType = economy.OfferMarket
+	} else {
+		if len(c) == 5 {
+			orderType = economy.OfferLimit
+			price, ok = parsePrice(c[4])
+			if !ok {
+				return 0, 0, "", 0, 0, false
+			}
+		} else {
+			fmt.Printf("Invalid offer %+v", c)
+			return 0, 0, "", 0, 0, false
+		}
+	}
+	symbol := economy.Symbol(c[1])
+	account, ok := parseAccount(c[0])
+	if !ok {
+		return 0, 0, "", 0, 0, false
+	}
+	amount, ok := parseAmount(c[2])
+	if !ok {
+		return 0, 0, "", 0, 0, false
+	}
+	return orderType, account, symbol, amount, price, true
+}
+
+func showMarket(market *economy.Market) {
+	symbols := market.AllSymbols()
+	fmt.Println("Symbol Last Price")
+	for _, symbol := range symbols {
+		price := market.LastPrice(symbol)
+		fmt.Printf("%6s %10d\n", symbol, price)
+	}
+}
+
+func parseAccount(in string) (int64, bool) {
+	return parseInt64(in, "Account ID must be an int64")
+}
+
+func parseAmount(in string) (int64, bool) {
+	return parseInt64(in, "Amount must be an int64")
+}
+
+func parsePrice(in string) (int64, bool) {
+	return parseInt64(in, "Price must be an int64")
+}
+
+func parseInt64(in, msg string) (int64, bool) {
+	id, err := strconv.ParseInt(in, 10, 64)
+	if err != nil {
+		fmt.Println(err.Error())
+		fmt.Println(msg)
+		return 0, false
+	}
+	return id, true
+}
